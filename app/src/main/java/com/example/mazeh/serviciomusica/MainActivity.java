724d69112f860fff39eb41f98a2c7acc7bd91dfb
Jose <mazehunter7@hotmail.com>
2019-03-09 19:55:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mazeh/serviciomusica/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.mazeh.serviciomusica;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+        Button arrancar = (Button) findViewById(R.id.boton_arrancar);
+
+        arrancar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                startService(new Intent(MainActivity.this,
+
+                        ServicioMusica.class));
+
+            }
+
+        });
+
+        Button detener = (Button) findViewById(R.id.boton_detener);
+
+        detener.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                stopService(new Intent(MainActivity.this,
+
+                        ServicioMusica.class));
+
+            }
+
+        });
+
+    }
+
+}
